14c58684251a6ad1722a67459fc930820f0d5dd9
Codeofsanju <sanjeev.sharma90@myhunter.cuny.edu>
2018-05-25 00:04:39 -0400

    435 - Project

---
+++ InsertToTables.sql
@@ -0,0 +1,143 @@
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (1,'ACTIVE', 8, 'Influeza', 5);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (2,'ACTIVE', 5, 'Asthma', 6);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (3,'ACTIVE', 3, 'Pnuemonia', 5);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (4,'ACTIVE', 7, 'Tuberculosis', 1);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (5,'ACTIVE', 2, 'Chickenpox', 2);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (6,'INACTIVE', 9, 'Measeles', 3);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (7,'INACTIVE', 10, 'Malaria', 4);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (8,'INACTIVE', 5, 'Meningitis', 4);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (9,'INACTIVE', 2, 'Commone Cold', 2);
+INSERT INTO Object (OBJECT_NUM, BREACH_STATUS, DANGER_LEVEL, DESCRIPTION, DIFFICULTY) VALUES (10,'INACTIVE', 2, 'Rubella', 1);
+
+-- Insert into Employeee_Info
+    --For Researchers
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(748388473, 'Rifat', 'Noor', 'NYC', 22, '646-546-0000');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(893486023, 'Bobby', 'Jindal', 'Las Vegas', 40, '646-546-0001');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(098124654, 'Sanjeev', 'Sharma', 'NYC', 25, '646-546-0002');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(982346872, 'Danny', 'Brown', 'Dallas', 19, '646-546-0004');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(092834879, 'Chrisy', 'Hernandez', 'Dallas', 24, '646-546-0005');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(098123402, 'Michelle', 'Shahid', 'NYC', 48, '646-546-0003');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(462729223, 'Dennis', 'Henry', 'Las Vegas', 32, '646-546-0006');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(040858933, 'LaQuisha', 'Brownlee', 'Mobile', 36, '646-546-0007');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(487263947, 'Zara', 'Imtiaz', 'Houston', 23, '646-546-0008');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(089230938, 'Henry', 'Hudson', 'New York City', 62, '646-546-0009');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(109238409, 'Chris', 'Hernan', 'Dallas', 26, '646-546-00010');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(348975878, 'Mich', 'Shah', 'NYC', 43, '646-546-0011');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(438590943, 'Dan', 'Hen', 'Las Vegas', 31, '646-546-0012');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(987654334, 'La', 'Lee', 'Mobile', 36, '646-546-0013');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(989324997, 'Za', 'Tiaz', 'Houston', 43, '646-546-0014');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(980967898, 'Enry', 'Son', 'New York City', 62, '646-546-0015');
+
+    --For Directors
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(897420482, 'Brian', 'Sweeny', 'NYC', 34, '646-546-0016');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(872094293, 'George', 'Washington', 'Las Vegas', 51, '646-546-0017');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(983542098, 'Lee', 'Leam', 'Mobile', 29, '646-546-0018');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(672873734, 'Zeta', 'Feliz', 'Houston', 93, '646-546-0019');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(039847832, 'Thurmon', 'Herman', 'New York City', 54, '646-546-0020');
+
+--For Agents
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(752845293, 'Chiboukem', 'Izwakum', 'Atlanta', 41, '647-346-0216');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(843758733, 'Kaneki', 'Ken', 'Minnesota', 21, '123-544-3017');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(827483823, 'Reggie', 'Robinson', 'Detroit', 37, '583-245-4235');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(282940334, 'Xong-ki', 'Ku', 'San Antonio', 56, '359-964-1543');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(034938533, 'Yasmeen', 'Sarker', 'NYC', 57, '955-453-9544');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(284938453, 'Saigenesh', 'Ravikumar', 'NYC', 31, '0556-295-9543');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(583938234, 'Jacob', 'Jacobs', 'Pittsburg', 40, '995-0954-4955');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(958439443, 'Khanisha', 'Jenkins', 'NYC', 26, '095-0666-0543');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(449345432, 'Alisha', 'Giles', 'Houston', 33, '095-0954-9543');
+INSERT INTO EMPLOYEE_INFO(SSN,Fname,Lname,Address, Age, Phone) VALUES(294543034, 'Andrew', 'Nathan', 'NYC', 24, '054-134-0543');
+
+
+--Insert Researchers
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(1,'Senior', 4, 748388473);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(2,'Junior', 2, 893486023);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(3,'Senior', 1, 098124654);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(4,'Intern', 2, 982346872);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(5,'Junior', 3, 092834879);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(6,'Junior', 1, 462729223);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(7,'Intern', 5, 040858933);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(8,'Associate', 6, 487263947);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(9,'Associate', 1, 098123402);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(10,'Associate', 5, 089230938);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(11, 'Senior' , 10 , 109238409);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(12, 'Junior' , 10 , 348975878);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(13, 'Senior' , 9 , 438590943);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(14, 'Associate' , 8 , 987654334);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(15, 'Associate' , 8 , 989324997);
+INSERT INTO RESEARCHERS(ID, Position, ObjectNum, SSN) VALUES(16, 'Junior' , 7 , 980967898);
+
+
+
+--Insert Departments
+INSERT INTO DEPARTMENTS(Dnum, Dname, Description, Location, Phone) VALUES(1,'Sexual Diseases', 'Investigate Sexual Diseases', 'NYC', '123-456-7841');
+INSERT INTO DEPARTMENTS(Dnum, Dname, Description, Location, Phone) VALUES(2,'Viral Diseases', 'Investigate Viral Diseases', 'Dallas', '123-456-7231');
+INSERT INTO DEPARTMENTS(Dnum, Dname, Description, Location, Phone) VALUES(3,'Mental Diseases', 'Investigate Mental Diseases', 'Las Vegas', '123-456-1321');
+INSERT INTO DEPARTMENTS(Dnum, Dname, Description, Location, Phone) VALUES(4,'Genetic Diseases', 'Investigate Genetic Diseases', 'NYC', '123-456-3241');
+INSERT INTO DEPARTMENTS(Dnum, Dname, Description, Location, Phone) VALUES(5,'Random Diseases', 'Investigate Random Diseases', 'Houston', '123-456-1233');
+--Insert Directors
+INSERT INTO DIRECTORS(ID, Dnum, SSN) VALUES(1, 1, 897420482);
+INSERT INTO DIRECTORS(ID, Dnum, SSN) VALUES(2, 2, 872094293);
+INSERT INTO DIRECTORS(ID, Dnum, SSN) VALUES(3, 3, 983542098);
+INSERT INTO DIRECTORS(ID, Dnum, SSN) VALUES(4, 4, 672873734);
+INSERT INTO DIRECTORS(ID, Dnum, SSN) VALUES(5, 5, 039847832);
+
+
+--Insert Sites
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(1, 'NYC', '343-424-3273', 'Good', 1);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(2, 'NYC', '590-432-3583', 'Good', 1);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(3, 'NYC', '943-423-3901', 'Bad', 4);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(4, 'NYC', '902-9023-3456', 'Great', 4);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(5, 'Dallas', '234-654-9394', 'Terrible', 2);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(6, 'Dallas', '934-945-1294', 'Great', 2);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(7, 'Las Vegas', '199-333-2222', 'Bad', 3);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(8, 'Las Vegas', '122-111-5555', 'Good', 3);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(9, 'Houston', '893-982-9822', 'Great', 5);
+INSERT INTO SITES(SiteNum, Location, Phone, Status, Dnum) VALUES(10, 'Houston', '432-343-3545', 'Good', 5);
+
+--Insert Research Logs
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(1, '2016-10-17', 'Knives', 'Success', 1, 3);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(2, '2016-10-27', 'Microscopes', 'Success', 2, 2);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(3, '2016-11-22', 'Boiler Plate', 'Ongoing', 3, 5);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(4, '2016-10-18', 'Graduated Cylinder', 'Failure', 4, 1);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(5, '2014-09-23', 'Hammer', 'Failure', 5, 7);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(6, '2016-05-02', 'Centrifuge', 'Suspended', 6, 8);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(7, '2016-07-23', 'Oven', 'Success', 7, 16);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(8, '2016-07-13', 'Laser Scope', 'Success', 8, 14);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(9, '2015-07-11', 'Microscopes', 'Ongoing', 9, 13);
+INSERT INTO RESEARCH_LOG(LogNum, LogDate, Equipment, Result, ObjectNum, Researcher_ID) VALUES(10, '2016-01-03', 'Centrifuge', 'Failure', 10, 11);
+
+--Insert Breach Logs
+
+INSERT INTO BREACH_LOG(BreachNum, BreachDate, Description, NumAffected, ObjectNum, DIRECTOR_ID) VALUES(1, '2017-10-27', 'Disease got out', 232, 1, 1);
+INSERT INTO BREACH_LOG(BreachNum, BreachDate, Description, NumAffected, ObjectNum, DIRECTOR_ID) VALUES(2, '2016-11-21', 'Stolen', 1232, 2, 2);
+INSERT INTO BREACH_LOG(BreachNum, BreachDate, Description, NumAffected, ObjectNum, DIRECTOR_ID) VALUES(3, '2016-12-24', 'Mutation', 134543, 3, 3);
+INSERT INTO BREACH_LOG(BreachNum, BreachDate, Description, NumAffected, ObjectNum, DIRECTOR_ID) VALUES(4, '2017-01-15', 'Disease got out', 5, 4, 4);
+INSERT INTO BREACH_LOG(BreachNum, BreachDate, Description, NumAffected, ObjectNum, DIRECTOR_ID) VALUES(5, '2018-04-20', 'Stolen', 387, 5, 5);
+
+
+
+
+--Insert Agents
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(1,'Alpha-09', 1, 1, 752845293);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(2,'Beta-09', 1, 2, 843758733);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(3,'Alpha-07', 2, 3, 827483823);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(4,'Theta-09', 2, 4, 282940334);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(5,'Beta-09', 3, 5, 034938533);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(6,'Alpha-09', 3, 6, 284938453);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(7,'Alpha-07', 4, 7, 583938234);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(8,'Beta-09', 4, 8, 958439443);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(9,'Alpha-09', 5, 9, 449345432);
+INSERT INTO AGENTS(ID, MTF_Num, Dnum, ObjectNum, SSN) VALUES(10,'Beta-09', 5, 10, 294543034);
+
+INSERT INTO MISSION(MissionNum, Status, Description,  ObjectNum, AGENT_ID) VALUES(1, 'Complete', 'Capture b-2 virus', 1, 1);
+INSERT INTO MISSION(MissionNum, Status, Description,  ObjectNum, AGENT_ID) VALUES(2, 'Complete', 'Capture c-3 virus', 2, 3);
+INSERT INTO MISSION(MissionNum, Status, Description,  ObjectNum, AGENT_ID) VALUES(3, 'Complete', 'Capture f9 virus', 3, 5);
+INSERT INTO MISSION(MissionNum, Status, Description,  ObjectNum, AGENT_ID) VALUES(4, 'Complete', 'Capture z9 predator', 4, 7);
+INSERT INTO MISSION(MissionNum, Status, Description,  ObjectNum, AGENT_ID) VALUES(5, 'Complete', 'Capture k2 zombie', 5, 9);
+
+
+
+
+
+
+
